cef4d8b50bb6472ddbac6156443bc75ae87e287a
Carlos Eduardo <cadu.eduardo1@outlook.com>
2018-08-07 09:20:05 -0300

    Feita alterações na forma que é salvo e alterado os dados

--- EcommerceOsorioManha/EcommerceOsorioManha/Controllers/ProdutoController.cs
+++ EcommerceOsorioManha/EcommerceOsorioManha/Controllers/ProdutoController.cs
@@ -16,25 +16,16 @@ namespace EcommerceOsorioManha.Controllers
         // GET: Produto
         public ActionResult Index()
         {
-            ViewBag.Data = DateTime.Now;
-            ViewBag.Produtos = ProdutoDAO.RetornarProdutos();
-            return View();
+            ViewBag.Data = DateTime.Now;            
+            return View(ProdutoDAO.RetornarProdutos());
         }
         public ActionResult CadastrarProduto()
         {
             return View();
         }
         [HttpPost]
-        public ActionResult CadastrarProduto(string txtNome, string txtDescricao, string txtPreco, string txtCategoria)
+        public ActionResult CadastrarProduto(Produto produto)
         {
-            Produto produto = new Produto()
-            {
-                Nome = txtNome,
-                Descricao = txtDescricao,
-                Preco = Convert.ToDouble(txtPreco),
-                Categoria = txtCategoria
-            };
-
             ProdutoDAO.CadastrarProduto(produto);
             return RedirectToAction("Index", "Produto");
 
@@ -50,24 +41,22 @@ namespace EcommerceOsorioManha.Controllers
 
         public ActionResult AlterarProduto(int? id)
         {
-            ViewBag.Produto = ProdutoDAO.BuscarProdutoPorId(id);
-            return View();
+            return View(ProdutoDAO.BuscarProdutoPorId(id));
         }
         [HttpPost]
-        public ActionResult AlterarProduto(string txtNome, string txtDescricao, string txtPreco, string txtCategoria, int txtId)
+        public ActionResult AlterarProduto(Produto produtoAlterado)
         {
-            Produto produto = ProdutoDAO.BuscarProdutoPorId(txtId);
+            Produto produtoOriginal = 
+            ProdutoDAO.BuscarProdutoPorId(produtoAlterado.ProdutoId);
 
+            produtoOriginal.Nome = produtoAlterado.Nome;
+            produtoOriginal.Descricao = produtoAlterado.Descricao;
+            produtoOriginal.Preco = produtoAlterado.Preco;
+            produtoOriginal.Categoria= produtoAlterado.Categoria;
 
-            produto.Nome = txtNome;
-            produto.Descricao = txtDescricao;
-            produto.Preco = Convert.ToDouble(txtPreco);
-            produto.Categoria = txtCategoria;
-
-            ProdutoDAO.AlterarProduto(produto);           
+            ProdutoDAO.AlterarProduto(produtoOriginal);
 
             return RedirectToAction("Index", "Produto");
-
         }
 
 
